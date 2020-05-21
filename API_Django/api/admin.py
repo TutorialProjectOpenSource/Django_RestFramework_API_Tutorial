039b87b07d9724002c5990c65fd6493ea3420859
Amir Mohammad Kargar <a.m.kargar2000@gmail.com>
2020-05-21 14:25:06 +0430

    Make models
    Make models and connect to server

--- API_Django/api/admin.py
+++ API_Django/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from  .models import api_data
 
-# Register your models here.
+class api_data_property(admin.ModelAdmin):
+    list_display=['title','category','color','size']
+
+admin.site.register(api_data,api_data_property)
--- API_Django/api/models.py
+++ API_Django/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class api_data(models.Model):
+    title=models.CharField(max_length=100)
+    category=models.CharField(max_length=150)
+    color=models.CharField(max_length=100)
+    size=models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title    
